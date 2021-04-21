8528df832bd619b1073d62755cf38b9b0d8eb6a4
crisgarm <crisgarm9@gmail.com>
2021-04-21 16:16:20 +0200

    Create a new property for username

--- script.js
+++ script.js
@@ -81,5 +81,18 @@ const displayMovements = function (movements) {
 
 displayMovements(account1.movements);
 
+const createUsernames = function (accs) {
+  accs.forEach(account => {
+    account.username = account.owner
+      .toLowerCase()
+      .split(' ')
+      .map(word => word[0])
+      .join('');
+  });
+};
+
+createUsernames(accounts);
+console.log(accounts);
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
